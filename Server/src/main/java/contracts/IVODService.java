6acecae6b1f123b8c5b2adb076fdab0f2f89c384
Theophile-Yvars <theo.yvars@gmail.com>
2022-09-28 17:49:46 +0200

    [DEV] VODService Remote #2

--- Server/src/main/java/contracts/IVODService.java
+++ Server/src/main/java/contracts/IVODService.java
@@ -2,9 +2,11 @@ package contracts;
 
 import contracts.movies.MovieDesc;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
-public interface IVODService {
-    List<MovieDesc> viewCatalog();
-    void playmovie(String isbn);
+public interface IVODService extends Remote {
+    List<MovieDesc> viewCatalog() throws RemoteException;
+    void playmovie(String isbn) throws RemoteException;
 }
--- Server/src/main/java/contracts/VODService.java
+++ Server/src/main/java/contracts/VODService.java
@@ -2,9 +2,15 @@ package contracts;
 
 import contracts.movies.MovieDesc;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
-public class VODService implements IVODService{
+public class VODService extends UnicastRemoteObject implements IVODService{
+    protected VODService(int port) throws RemoteException {
+        super(port);
+    }
+
     @Override
     public List<MovieDesc> viewCatalog() {
         return null;
