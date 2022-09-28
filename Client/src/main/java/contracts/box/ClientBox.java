8f0b5a4e1ce3c6880eafee10c388a13fe0c4fd2c
Theophile-Yvars <theo.yvars@gmail.com>
2022-09-28 18:45:36 +0200

    [DEV] ClientBox init #5

---
+++ Client/src/main/java/contracts/box/ClientBox.java
@@ -0,0 +1,17 @@
+package contracts.box;
+
+import contracts.IClientBox;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ClientBox extends UnicastRemoteObject implements IClientBox {
+    public ClientBox(int port) throws RemoteException {
+        super(port);
+    }
+
+    @Override
+    public void stream(byte[] chunk) throws RemoteException{
+        System.out.println("Le film ..... le octect");
+    }
+}
