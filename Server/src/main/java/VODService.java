1a007dbf828fb5d6f341a7fb31e694cadf960444
JulesPrince <jules.prince.fr@iclpud.com>
2022-09-28 18:24:11 +0200

    [FIX] enable client to choose a movie #2

--- Server/src/main/java/VODService.java
+++ Server/src/main/java/VODService.java
@@ -1,24 +1,53 @@
 import contracts.IVODService;
 import contracts.movies.MovieDesc;
+import lombok.Getter;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 public class VODService extends UnicastRemoteObject implements IVODService {
-    protected VODService(int port) throws RemoteException {
+
+    private List<MovieDesc> catalog;
+    public VODService(int port) throws RemoteException {
         super(port);
+
+        this.catalog = new ArrayList<>();
+
+        this.initialize();
+    }
+
+    private void initialize(){
+        this.catalog.add(new MovieDesc("Chucky", "1234", "Une poupée tueuse"));
     }
 
     @Override
     public List<MovieDesc> viewCatalog() {
-        return List.of(new MovieDesc("Chucky", "1234", "Une poupée tueuse"));
+        return this.getCatalog();
+    }
+
+    private MovieDesc searchMovieByIbsn(String ibsn){
+        for(MovieDesc movie : this.getCatalog()){
+            if(movie.getIsbn().equals(ibsn))
+                return movie;
+        }
+
+        return null;
     }
 
     @Override
     public void playmovie(String isbn) {
-
+        MovieDesc movieRequested = this.searchMovieByIbsn(isbn);
+        if(movieRequested != null){
+            System.out.println("Le client demande à voir le film" + movieRequested.getMovieName());
+        }
+        else {
+            //TODO implements next error
+            System.out.println("LE FILM DU CLIENT N'EST PAS CONNUS DU CATALOGUE !!!");
+        }
     }
 }
