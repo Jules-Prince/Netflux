be7b7acaf73349df963fe6d64b394fbfc0cf2729
Theophile-Yvars <theo.yvars@gmail.com>
2022-09-28 17:46:43 +0200

    [DEV] Create VODService  #2

---
+++ Server/src/main/java/contracts/VODService.java
@@ -0,0 +1,17 @@
+package contracts;
+
+import contracts.movies.MovieDesc;
+
+import java.util.List;
+
+public class VODService implements IVODService{
+    @Override
+    public List<MovieDesc> viewCatalog() {
+        return null;
+    }
+
+    @Override
+    public void playmovie(String isbn) {
+
+    }
+}
