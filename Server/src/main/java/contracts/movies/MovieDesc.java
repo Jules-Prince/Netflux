6c33af74e18dac985d917c433248b8914e8a051f
JulesPrince <jules.prince.fr@iclpud.com>
2022-09-28 17:41:09 +0200

    Create movie desc #3

--- Server/src/main/java/contracts/movies/MovieDesc.java
+++ Server/src/main/java/contracts/movies/MovieDesc.java
@@ -1,4 +1,19 @@
 package contracts.movies;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MovieDesc {
+
+    private String movieName;
+    String isbn;
+    String synopsis;
+
+    public MovieDesc(String movieName, String isbn, String synopsis) {
+        this.movieName = movieName;
+        this.isbn = isbn;
+        this.synopsis = synopsis;
+    }
 }
