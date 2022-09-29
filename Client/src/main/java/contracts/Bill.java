556a5e40e6158fea18d2608770b7b1cb57567a82
Theophile-Yvars <theo.yvars@gmail.com>
2022-09-29 18:42:55 +0200

    [DEV] The customer collects the bill #7

--- Client/src/main/java/contracts/Bill.java
+++ Client/src/main/java/contracts/Bill.java
@@ -1,2 +1,17 @@
-package contracts;public class Bill {
+package contracts;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+@Getter
+public class Bill implements Serializable {
+    private String movieName;
+    private BigInteger outrageousPrice;
+
+    public Bill(String movieName, BigInteger outrageousPrice) {
+        this.movieName = movieName;
+        this.outrageousPrice = outrageousPrice;
+    }
 }
