f2832dfdf047ccf385033c3df5aee1e4ea74f28a
Theophile-Yvars <theo.yvars@gmail.com>
2022-10-03 18:52:41 +0200

    [DEV] Des films en plus  #12

--- Client/src/main/java/contracts/box/ClientBox.java
+++ Client/src/main/java/contracts/box/ClientBox.java
@@ -6,12 +6,20 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ClientBox extends UnicastRemoteObject implements IClientBox {
+    private int display;
+
     public ClientBox(int port) throws RemoteException {
         super(port);
+        this.display = 0;
     }
 
     @Override
     public void stream(byte[] chunk) throws RemoteException{
         System.out.print(chunk + " ");
+        this.display = this.display + 1;
+        if(this.display == 6){
+            this.display = 0;
+            System.out.println("\n");
+        }
     }
 }
